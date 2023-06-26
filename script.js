d03ced383fd10747d5194c8998ef267bab3a8a0a
Grasiela Texeira <grasy.mst@gmail.com>
2023-06-26 10:40:52 -0300

    Events

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 // Initial Data
 let currentColor = 'black';
+let canDraw = false;
 
 let screen = document.querySelector('#tela');
 let ctx = screen.getContext('2d');
@@ -11,10 +12,24 @@ document.querySelectorAll('.colorArea .color').forEach(item => {
 
 });
 
+screen.addEventListener('mousedown', mouseDownEvent);
+screen.addEventListener('mousemove', mouseMoveEvent);
+screen.addEventListener('mouseup', mouseUpEvent);
+
+// Functions
 function colorClickEvent(e){
     let color = e.target.getAttribute('data-color');
     currentColor = color;
 
     document.querySelector('.color.active').classList.remove('active');
     e.target.classList.add('active');
+}
+function mouseDownEvent(){
+
+}
+function mouseMoveEvent(){
+
+}
+function mouseUpEvent(){
+
 }
